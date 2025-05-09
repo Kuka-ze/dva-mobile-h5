a2b594c7a32e192406b351b4df30adaa28a3617e
帅泽 <vuejs1024@163.com>
2025-05-09 15:55:26 +0800

    commit new function

--- src/utils/common.js
+++ src/utils/common.js
@@ -10,8 +10,30 @@ function debounce(func, delay) {
     };
 }
 
+
+const throttle = (func, delay) => {
+    let last, deferTimer //闭包中的自由变量，表示上一次执行的时间，以及定时器
+    return (args) => {
+        // 干掉触发
+        let now = +new Date();
+        if (last && now < last + delay) {
+            clearTimeout(deferTimer)
+            deferTimer = setTimeout(() => {
+                last = now
+                func(args)
+            }, delay)
+        } else {
+            //需要让执行过段时间再来到此处
+            last = now  // 第一次时间
+            func(args)  // 先执行一次
+        }
+    }
+}
+
+
 export {
-    debounce
+    debounce,
+    throttle
 }
 
 // // 使用示例
